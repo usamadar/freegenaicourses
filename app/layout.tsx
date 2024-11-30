bc3c3696fde0464a016b4dfbd647ac546c1634d8
Usama Dar <usama.dar@westwing.dels>
2024-11-30 01:32:07 +0000

    fixed the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,7 +63,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <>
+    <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
@@ -93,7 +93,7 @@ export default function RootLayout({
           }}
         />
       </body>
-    </>
+    </html>
   )
 }
 
